fc0d4daabfc7a9d55946ce10f965bb6b680dba1a
hasandi_palliaguruge <pghasandi@gmail.com>
2019-09-14 19:28:39 +0530

    added parameters

--- src/app/department-details/department-details.component.ts
+++ src/app/department-details/department-details.component.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import {  ActivatedRoute}from '@angular/router'
+import {  ActivatedRoute, Router, ParamMap }from '@angular/router';
+
+ 
 @Component({
   selector: 'app-department-details',
   template: `
-    <p>
-      department-details works!
-    </p>
+  <h3>
+    you have selected department with id = {{departmentId}}
+  </h3>
+  <a (click)="goPrevious()">Previous </a>
+  <a (click)="goNext()">Next</a>
+
   `,
   styles: []
 })
 export class DepartmentDetailsComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  public departmentId;
+  constructor(private route: ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
-    let id=parseInt(this.route.snapshot.paramMap.get('id'));
+    // let id=parseInt(this.route.snapshot.paramMap.get('id'));
+    //from the current route snapshot get the id parameter from the url
+    // this.departmentId = id;
+    this.route.paramMap.subscribe((params:ParamMap)=>{
+      let id=parseInt(params.get('id'));
+      this.departmentId=id;
+
+    })
+    
   }
 
+goPrevious(){
+  let previousId=this.departmentId -1;
+  this.router.navigate(['/departments',previousId])
+}
+
+goNext(){
+  let nextId=this.departmentId +1;
+  this.router.navigate(['/departments',nextId])
+}
+  
 }
